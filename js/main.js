d741d3c037582172108ce021ec2d34fd226e292e
Behruz <xujamurodovbehruz@gmail.com>
2023-05-20 09:12:37 +0500

    js 5-dars
    js 5-dars kodlari yuklandi!

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*let headTag = document.head;
+let bodyTag = document.body;
+let havola = document.documentURI;*//*
+let formalar = document.forms;*/
+/*
+function battonniBos(){
+    alert("Siz battonni bir marta bosdingiz!")
+}
+
+function battonni2martaBos(){
+    alert("Siz battonni ikki marta bosdingiz")
+}
+
+function Tanlash(event){
+    alert("Siz "+ event.target.value + "ni tanladingiz!")
+}
+
+function inputgaBosish(){
+    console.log("Inputga bosildi!")
+}
+function inputdanChiqish(){
+    console.log("Inputdan chiqildi")
+}
+
+function yozish(event){
+    console.log(event.target.value)
+}
+document.querySelector("classNomi").addEventListener('copy', function (event){
+    console.log( "Diqqat! Sizdan" + event.target.value + "nomli fayllar ko'chirilmoqda!");
+})*/
+
+function harakatlanish(e){
+    console.log(e.clientX,e.clientY);
+}
+
+function kirish(){
+    console.log("Hududga kirish!")
+}
+function chiqish(){
+    console.log("Hududdan chiqildi!")
+}
